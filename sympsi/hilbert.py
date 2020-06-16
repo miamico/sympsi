730d30cadedd1a9c24d5a612eb16f13b266aa2a2
Mirko Amico <31739486+miamico@users.noreply.github.com>
2020-06-16 14:00:33 -0700

    Update hilbert.py

--- sympsi/hilbert.py
+++ sympsi/hilbert.py
@@ -8,7 +8,7 @@ Authors:
 from __future__ import print_function, division
 
 from sympy import Basic, Interval, oo, sympify
-from sympy.core.compatibility import u
+from sympy.core.compatibility import u_decode as u
 from sympy.printing.pretty.stringpict import prettyForm
 
 from sympsi.qexpr import QuantumError
